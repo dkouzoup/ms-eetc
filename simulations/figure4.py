e3deb4492956b46388284f96b02428ffc8bf0375
Dimitris Kouzoupis <dimitris.kouzoupis@fhnw.ch>
2023-03-09 13:50:00 +0100

    added heatmaps in fig. 4

--- simulations/figure4.py
+++ simulations/figure4.py
@@ -9,14 +9,13 @@ from utils import latexify, show, saveFig
 from efficiency import loadToForce, totalLossesFunction
 
 
-def plotSpline(loads, velocities, splineFun, forceMax, powerMax, axObj=None):
+def plotSpline(loads, velocities, splineFun, forceMax, powerMax, axObj=None, type='3d'):
 
     loads3D, velocities3D = np.meshgrid(loads, velocities, indexing='ij')
 
     forces3D = loadToForce(loads3D, velocities3D, forceMax, powerMax)
 
     losses3D = np.zeros(loads3D.shape)
-    eta3D = np.zeros(loads3D.shape)
 
     rows, cols = losses3D.shape
 
@@ -30,17 +29,29 @@ def plotSpline(loads, velocities, splineFun, forceMax, powerMax, axObj=None):
 
         axObj = plt.axes(projection='3d')
 
-    axObj.plot_surface(1e-3*forces3D, velocities3D*3.6, 1e-3*losses3D, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+    if type == '3d':
 
-    axObj.set_xlabel('Force [kN]')
-    axObj.set_ylabel('Velocity [km/h]')
-    axObj.set_zlabel('Losses [kW]')
+        handle = axObj.plot_surface(1e-3*forces3D, velocities3D*3.6, 1e-3*losses3D, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+        axObj.set_zlabel('Losses [kW]')
+        axObj.set_xlabel('Force [kN]')
+        axObj.set_ylabel('Velocity [km/h]')
+
+    elif type == 'heatmap':
+
+        handle = axObj.contourf(velocities3D*3.6, 1e-3*forces3D, 1e-3*losses3D)
+        axObj.set_xlabel('Velocity [km/h]')
+        axObj.set_ylabel('Force [kN]')
+        axObj.set_title('Losses [kW]')
+
+    else:
+
+        raise ValueError("Unknown type!")
 
     if axObj is None:
 
         show()
 
-    return np.amax(losses3D)
+    return np.amax(losses3D), handle
 
 
 def plotSplines(loads, velocities, splineFun1, splineFun2, forceMax, powerMax, plotEta=False, figSize=None, filename=None):
@@ -48,17 +59,26 @@ def plotSplines(loads, velocities, splineFun1, splineFun2, forceMax, powerMax, p
     latexify()
 
     fig = plt.figure()
-    ax1 = fig.add_subplot(1,2,1,projection='3d')
-    ax2 = fig.add_subplot(1,2,2,projection='3d')
+    ax1 = fig.add_subplot(2,2,1)
+    ax2 = fig.add_subplot(2,2,2)
+    ax3 = fig.add_subplot(2,2,3,projection='3d')
+    ax4 = fig.add_subplot(2,2,4,projection='3d')
 
-    lossesMax1 = plotSpline(loads, velocities, splineFun1, forceMax, powerMax, axObj=ax1)
-    lossesMax2 = plotSpline(loads, velocities, splineFun2, forceMax, powerMax, axObj=ax2)
+    lossesMax1, h1 = plotSpline(loads, velocities, splineFun1, forceMax, powerMax, axObj=ax1, type='heatmap')
+    lossesMax2, h2 = plotSpline(loads, velocities, splineFun2, forceMax, powerMax, axObj=ax2, type='heatmap')
+    plotSpline(loads, velocities, splineFun1, forceMax, powerMax, axObj=ax3, type='3d')
+    plotSpline(loads, velocities, splineFun2, forceMax, powerMax, axObj=ax4, type='3d')
 
     if figSize is not None:
 
         fig.set_size_inches(figSize[0], figSize[1])
 
     fig.tight_layout()
+
+    # move colorbar next to ax2
+    box = ax2.get_position()
+    cb = fig.add_axes([box.x1 + 0.03, box.y0, 0.02, box.y1-box.y0])
+    fig.colorbar(h2,cax=cb)
 
     saveFig(fig, [ax1, ax2], filename)
 
@@ -81,7 +101,7 @@ if __name__ == '__main__':
 
     etaMax = 0.73
 
-    lossesMax1, lossesMax2 = plotSplines(loadsEval, velocitiesEval, fun1, fun2, train.forceMax, train.powerMax, figSize=[8, 4], filename='figure4.pdf')
+    lossesMax1, lossesMax2 = plotSplines(loadsEval, velocitiesEval, fun1, fun2, train.forceMax, train.powerMax, figSize=[8, 7], filename='figure4.pdf')
 
     if not 0.99 <= lossesMax1/lossesMax2 <= 1.01:
 
